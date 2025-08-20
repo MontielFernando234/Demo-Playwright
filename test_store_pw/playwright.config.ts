05275ee583e064fea222ae409824c3090dc05386
unknown <montielfernandojavier@gmail.com>
2025-08-19 23:53:29 -0300

    improvement yml for deploy gh-pages

--- test_store_pw/playwright.config.ts
+++ test_store_pw/playwright.config.ts
@@ -14,24 +14,22 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [["line"], ["allure-playwright"]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
-
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
     baseURL: process.env.BASE_URL || 'https://automationteststore.com',
-    headless: process.env.HEADLESS ? process.env.HEADLESS === 'true' : false, // Run tests in headless mode by default
+    headless: process.env.HEADLESS ? process.env.HEADLESS === 'false' : true, // Run tests in headless mode by default
+    screenshot: 'only-on-failure',
   },
 
   /* Configure projects for major browsers */
